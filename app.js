be8a423eaa4f00af68ae1111fa029f5a40942218
chandresh-jain <cj.chandreshjain@gmail.com>
2021-05-20 18:12:42 +0530

    Done

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ var btnTranslate=document.querySelector("#btn-translate");
 var input=document.querySelector("#text-input");
 var outputDiv=document.querySelector("#output");
 
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(text)
+{
+    return serverURL + "?" + "text=" + text
+}
+
 
 function clickeventhandler()
 { 
-outputDiv.innerText="asasasa "+ input.value;
-};
+    var inputText= input.value;
+    fetch(getTranslationUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        translatedText=json.contents.translated;
+        outputDiv.innerText = translatedText;})
+
+
+    };
+    
+
 
 btnTranslate.addEventListener("click",clickeventhandler)
+
+
+
